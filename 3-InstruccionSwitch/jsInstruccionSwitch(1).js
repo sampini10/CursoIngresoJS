2fdf92c910f225a97577f8aa21cf507008bae9f1
alumno <alumno>
2020-02-14 19:55:08 -0300

    Clase3

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,20 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+function mostrar() {
+    //tomo la edad  
+    var mes = document.getElementById("mes").value;
+    switch (mes) {
+        case "Enero":
+            alert("Que comiences bien el año")
+            break;
+        case "Marzo":
+            alert("A clases!")
+            break;
+        case "Julio":
+            alert("Se vienen las vacaciones")
+            break;
+        case "Diciembre":
+            alert("Felices Fiestas")
+            break;
+    }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,10 +1,26 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
+function mostrar() {
+    //tomo la edad  
+    var mes = document.getElementById("mes").value;
+    switch (mes) {
+        case "Enero":
+        case "Febrero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+            alert("Falta para el invierno");
+            break;
+        case "Julio":
+        case "Agosto":
+            alert("Abrigate que hace frio");
+            break;
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+            alert("Pasamos el frio, ahora calor!");
+            break;
+    }
 
 
 

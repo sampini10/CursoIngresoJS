543b7f69900aac7ab1de85cf5ff4c07f89b52b41
alumno <alumno>
2020-02-14 21:06:42 -0300

    clase3

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,9 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById("mes").value;
+switch (mes){
+ case"Febrero":
+    alert("Este mes tiene 29 dias");
+    break;
+    default:
+    alert("Este mes tiene más de 30 dias");
+}
 
-//alert (mesDelAño);
 	
 	
 

228016c50c7adfd9492d66c5ee05c3890a36b40a
alumno <alumno>
2020-02-28 20:46:28 -0300

    ultimaclase

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,32 @@
-function mostrar()
-{
+function mostrar() {
+    var letra;
+    var numero;
+var seguir;
+var contadorPares = 0;
+var contadorImpares = 0;
+var contadorCeros = 0;
+var acumuladorPos = 0;
+var contadorPos = 0;
+var promedioPos;
+var acumuladorNeg = 0;
+var numeroMax;
+var numeroMin;
+var letraMax;
+var letraMin;
+var flag = 0;
 
+    do {
+        letra = prompt("Ingrese una letra: ");
+        while (!(letra >= "A" && letra <= "Z" || letra >= "a" && letra <= "z"));
+        letra = prompt("Eso no es una letra. Ingrese una letra : ");
+
+        numero = parseInt(prompt("Ingrese un numero (-100 y 100): "));
+        while (numero < -100 || numero > 100 || isNaN(numero)) {
+            numero = parseInt(prompt("Numero invalido. Ingrese un numero"));
+
+        }
+        seguir = prompt("Quiere continuar?:");
+    } while (seguir == "s");
+
+   
 }

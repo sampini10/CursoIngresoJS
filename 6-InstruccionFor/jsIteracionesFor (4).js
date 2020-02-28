391b5d957c56a3487252710c6cb7c055e140aeba
sampini10 <60760212+sampini10@users.noreply.github.com>
2020-02-27 21:04:04 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,7 +1,10 @@
-function mostrar()
-{
+function mostrar() {
+    for (var i = 0; i < 50; i++) {
+        if (i == 10) {
+            break;
+        } console.log(i);
+    }
 
 
 
-
-}//FIN DE LA FUNCIÓN
+}//si se agrega un for, el conteo va de 0 a 50, agregando un if con una condicion de que llegue hasta 10, se le agrega un break que corta el conteo hasta 50 
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,18 @@
-function mostrar()
-{
+function mostrar() {
+    var numero;
+    var contadorPares = 0;
+    numero = parseInt(prompt("Ingrese un numero: "));
+    while (isNaN(numero)) {
+        numero = parseInt(prompt("Eso no es un numero.Ingrese otro"));
+    }
+    for (var i = 1; i <= numero; i++) {
+        if (i % 2 == 0) {
+          //aca solo se entra cuando i tiene un valor par
+            console.log(i);
+            contadorPares++;
+        }
+    }
 
-
-
+console.log("Cantidad de pares encontrados: " + contadorPares);
 
 }//FIN DE LA FUNCIÓN

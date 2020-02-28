265be97d09ae5cac455a837ba6c18457f3ea859c
sampini10 <60760212+sampini10@users.noreply.github.com>
2020-02-27 22:16:27 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,36 @@
-function mostrar()
-{
+function mostrar() {
+    var nota;
+    var sexo;
+    var promedio;
+    var contadorVaronesMas6 = 0;
+    var acumuladorNotas = 0;
+    var notaBaja;
+    var sexoBajo;
+    var flag = 0;
 
-}
+    //pido datos repetitivos uso for
+    for (var i = 0; i < 5; i++) {
+        nota = parseInt(prompt("Ingrese nota"));
+        while (nota < 0 || nota > 10 || isNaN(nota)) {
+            nota = parent(prompt("Nota invalida"));
+        }
+        sexo = prompt("Ingrese sexo f-m");
+        while (sexo != "f" && sexo != "m") {
+            sexo = prompt("Sexo invalido");
+        }
+        if (flag == 0 || nota < notaBaja) {
+            notaBaja = nota;
+            sexoBajo = sexo
+            flag = 1;
+        }
+        acumuladorNotas = acumuladorNotas +nota;
+        if ( sexo == "m" && nota >= 6){
+            contadorVaronesMas6++;
+        }
+        }
+        promedio = acumuladorNotas / 5;
+        alert("Promedio notas " + promedio)
+
+
+
+    }

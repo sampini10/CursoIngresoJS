1771274b0808bc130a0fe86ada9bf5b000186019
alumno <alumno>
2020-02-28 22:16:53 -0300

    clasefinal

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,21 @@
-function mostrar()
-{
+function mostrar() {
+    var marca
+    var peso
+    var temperatura
+var seguir
 
+    do {
+        marca = prompt("Ingrese marca de producto");
+
+        peso = parseInt(prompt("Ingrese peso del producto (1-100kg):"))
+        while (peso < 1 || peso > 100 || isNaN(peso)) {
+            peso = parseInt(prompt("No es un peso valido"));
+        }
+        temperatura = parseInt(prompt("Ingrese temperatura de almacenamiento (-30 y 30 ºc"))
+        while (temperatura < -30 || temperatura > 30 || isNaN(temperatura)) {
+            temperatura = parseInt(prompt("No es una temperatura valida"));
+        }
+        seguir = prompt("Quiere seguir ingresando?: ")
+    
+    } while (seguir == "s");
 }
